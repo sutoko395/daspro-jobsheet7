e276b21b6f51bae4e66aa71276d384bd72f1244f
sutoko395 <sutokotokotoko@gmail.com>
2024-10-11 08:16:45 +0700

    perbarui file

--- SiakadFor23.java
+++ SiakadFor23.java
@@ -8,19 +8,30 @@ public class SiakadFor23 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jmlhLulus = 0, jmlhTidakLulus = 0,batasKelulusan = 60;
 
         for (int i = 1; i <= 10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ":");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= batasKelulusan) {
+                jmlhLulus++;
+            } else {
+                jmlhTidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+
+        System.out.println("Jumlah mahasiswa yang lulus: " + jmlhLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jmlhTidakLulus);
+
         sc.close();
     }
 }
